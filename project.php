d11e333a8287b1fd0e0e4c6dc34b8efc2bb20150
marlontamo2 <marlontamo2@gmail.com>
2024-08-05 15:45:27 +0800

    "added custom template and css"

---
+++ project.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Template Name: Task Template
+*/
+get_header();
+?>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-4">
+        <div class="card">
+            <div class="card-head text-center">
+                <h3>card 1</h3>
+            </div>
+            <div class="card-body">
+            <form>
+                <div class="form-group">
+                    <label for="formControlRange"></label>
+                    <input type="range" class="form-control-range range-custom" id="formControlRange">
+                </div>
+            </form>
+            </div>
+        </div>
+        </div>
+        <div class="col-md-4">
+        <div class="card">
+            <div class="card-head">
+                <h3>card 2</h3>
+            </div>
+            <div class="card-body">
+            <form>
+                <div class="form-group">
+                    <label for="formControlRange"> Range for card-2</label>
+                    <input type="range" class="form-control-range" id="formControlRange">
+                </div>
+            </form>
+            </div>
+        </div>
+        </div>
+        <div class="col-md-4">
+        <div class="card">
+            <div class="card-head">
+                <h3>card 3</h3>
+            </div>
+            <div class="card-body">
+            <form>
+                <div class="form-group">
+                    <label for="formControlRange"> Range for card-2</label>
+                    <input type="range" class="form-control-range" id="formControlRange">
+                </div>
+            </form>
+            </div>
+        </div>
+        </div>
+        
+    </div>
+</div>
+<?php get_footer(); ?>
