86c9d28ce6729690d1363c6f2397a3f56c9e3418
marlontamo2 <marlontamo2@gmail.com>
2024-08-05 17:23:47 +0800

    onload

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,17 @@
+$(document).ready(function() {
+    //card1
+    let slider1 = document.getElementById('formControlRange1').value;
+    let result1 = document.getElementById('resultForm1');
+      result1.innerHTML = slider1;
+    //card2
+    let slider2 = document.getElementById('formControlRange2').value;
+    let result2 = document.getElementById('resultForm2');
+      result2.innerHTML = slider2;
+      //card3
+      let slider3 = document.getElementById('formControlRange3').value;
+      let result3 = document.getElementById('resultForm3');
+        result3.innerHTML = slider3;
+});
 //result form1
 jQuery('#formControlRange1').on('change',function(){ 
     let slider = document.getElementById('formControlRange1').value;
