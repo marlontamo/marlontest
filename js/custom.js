c82cadc775b62349f5be9a42dca4d54b2030bd45
marlontamo2 <marlontamo2@gmail.com>
2024-08-05 16:31:31 +0800

    cust

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
 jQuery('#formControlRange1').on('change',function(){ 
-    let range = jQuery('#forControlRange1').val();
-      alert(range);
+    //let range = jQuery('#forControlRange1').val();
+      alert(this.val());
     });
