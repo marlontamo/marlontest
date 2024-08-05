67df261f49be390c2590ab775808a9fb00710a4c
marlontamo2 <marlontamo2@gmail.com>
2024-08-05 16:58:49 +0800

    innerhtml

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 jQuery('#formControlRange1').on('change',function(){ 
     let slider = document.getElementById('formControlRange1').value;
     let result = document.getElementById('result');
-      result.text = slider;
+      result.innerHTML = slider;
       console.log(slider);
 
     });
